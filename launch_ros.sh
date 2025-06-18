f476f842f29e74429373520395554cd49d831d75
Riley Mark <98252064+rileymark@users.noreply.github.com>
2025-06-17 21:18:08 -0500

    added daedalus service file and launch script

---
+++ launch_ros.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source /home/<TODO: Elias put username here>/catkin_ws/devel/setup.bash
+
+# TODO: Elias put that one command that you have to do before launching ros here:
+sudo this_is_the_command
+
+# TODO: Elias verify that this is the right launch file
+roslaunch daedalus_core mission.launch
